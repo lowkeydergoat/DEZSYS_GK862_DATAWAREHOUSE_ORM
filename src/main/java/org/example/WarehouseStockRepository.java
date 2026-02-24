553f0a1b598b21877e6d4ef63f5a2671e28a7799
stefan <skordov@student.tgm.ac.at>
2026-02-24 14:51:33 +0100

    fehler verbessert

--- src/main/java/org/example/WarehouseStockRepository.java
+++ src/main/java/org/example/WarehouseStockRepository.java
@@ -3,6 +3,6 @@ package org.example;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WarehouseStockRepository
-        extends CrudRepository<WarehouseStock, Integer> {
+        extends CrudRepository<WarehouseStock, String> {
 
 }
